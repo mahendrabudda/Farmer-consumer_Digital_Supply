6109a15e8975f780f6fdb1eb269709b615af993c
mahendrabudda <buddamahendra05@gmail.com>
2026-03-07 21:58:43 +0530

    Fix3 CORS configuration

--- server/server.js
+++ server/server.js
@@ -16,10 +16,8 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
-  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
-  allowedHeaders: ["Content-Type","Authorization"]
+  origin: true,
+  credentials: true
 }));
 // Middleware
 app.use(express.json());
