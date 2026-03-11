32667d43918830387a42986739e536fda8ceabc4
mahendrabudda <buddamahendra05@gmail.com>
2026-03-11 18:19:07 +0530

    another try for cors error

--- server/config/Nodemailer.js
+++ server/config/Nodemailer.js
@@ -6,6 +6,7 @@ const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
     port: 587,
     secure: false, // Use false for 587, true for 465
+    requireTLS: true,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
